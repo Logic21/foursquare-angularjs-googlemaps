16f099cc55bd469d419cee8737213955d5c77033
Ivan Proskuryakov <volgodark@gmail.com>
2018-11-13 21:13:33 +0300

    cleaning

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,9 @@
-'use strict';
+'use strict'
 
 define([
     'angular',
     'jQuery',
     'underscore',
-    'angular-google-analytics',
     'angular-resource',
     'angular-cookies',
     'angular-sanitize',
@@ -13,18 +12,13 @@ define([
     'ngMessages',
     'ngAria',
     'ngAnimate',
-    'angular-loading-bar',
     'angular-moment',
     'ngMaterial',
-    'infinite-scroll',
-    'ngFileUpload',
     'ngTagsInput',
-    'uiCropper'
   ],
   function (angular) {
 
     var app = angular.module('app', [
-      'angular-google-analytics',
       'ngMaterial',
       'ngCookies',
       'ngResource',
@@ -34,31 +28,22 @@ define([
       'ngMessages',
       'ngAria',
       'ngAnimate',
-      'angular-loading-bar',
       'angularMoment',
-      'infinite-scroll',
-      'ngFileUpload',
       'ngTagsInput',
-      'uiCropper',
-    ]);
+    ])
 
-    app
-      .config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
-        cfpLoadingBarProvider.includeSpinner = false;
-        cfpLoadingBarProvider.includeBar = true;
-      }])
-      .config(function ($provide, $locationProvider, $httpProvider) {
-        $httpProvider.defaults.withCredentials = true;
-        $locationProvider.html5Mode(true);
-      })
-      .config(['$qProvider', function ($qProvider) {
-        $qProvider.errorOnUnhandledRejections(false);
-      }])
-      .run(['$http', '$rootScope', 'initService', 'Analytics',
-        function ($http, $rootScope, initService, Analytics) {
-          initService.launch();
-        }
-      ]);
+    app.config(function ($provide, $locationProvider, $httpProvider) {
+      $httpProvider.defaults.withCredentials = true
+      $locationProvider.html5Mode(true)
+    }).config([
+      '$qProvider', function ($qProvider) {
+        $qProvider.errorOnUnhandledRejections(false)
+      }]).run([
+      '$http', '$rootScope', 'initService',
+      function ($http, $rootScope, initService) {
+        initService.launch()
+      },
+    ])
 
-    return app;
-  });
+    return app
+  })
